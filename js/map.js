3945e1e8fcdc93a0751d7a4e0606af63dfd9f71e
Soofeel Kim <rlatnvlf88@gmail.com>
2017-08-12 23:52:19 +0900

    Add Map container to index.html for showing map. Make map.html for using daum map api.

---
+++ js/map.js
@@ -0,0 +1,15 @@
+var container = document.getElementById('map');
+var options = {
+    center: new daum.maps.LatLng(37.404230, 127.111731),
+    level: 4
+};
+
+var map = new daum.maps.Map(container, options);
+
+var zoomControl = new daum.maps.ZoomControl();
+map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
+
+var marker = new daum.maps.Marker({
+    position: map.getCenter()
+});
+marker.setMap(map);
