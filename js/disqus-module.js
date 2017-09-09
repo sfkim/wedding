f001a4648674fd3ea84f91e90885c3573b028e34
Soofeel Kim <rlatnvlf88@gmail.com>
2017-09-09 14:17:41 +0900

    Change disqus module src. Add blank between gallery and disqus.

--- js/disqus-module.js
+++ js/disqus-module.js
@@ -9,7 +9,7 @@ var disqus_config = function () {
 
 (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    s.src = 'https://eunlovefeel.disqus.com/embed.js';
+    s.src = 'https://sfwedding.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
 })();
